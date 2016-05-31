f5a5b2fe34b95768912a48723d82d989f642c1b4
VarchukVladimir <fildes@gmail.com>
2016-05-31 04:38:10 +0300

    update

--- concat_episodes.py
+++ concat_episodes.py
@@ -3,6 +3,7 @@ import sys
 from utils import exec_subproc
 
 
+fps = 60
 
 
 def get_conctac_cmd(path, ep_index, frame_index, video_type):
@@ -26,6 +27,35 @@ def get_conctac_cmd(path, ep_index, frame_index, video_type):
     return cmd
 
 
+def duplicate_images(image_path, episode, min_frame, max_frame, type_):
+    path = '/'.join(image_path.split('/')[:-1])
+    if type_ == 0:
+        for i in range(max(0, min_frame - fps * 2), min_frame):
+            save_path = path + '/img_{0:03d}_{1:05d}.jpg'.format(episode, int(i))
+            cmd = ['cp', image_path, save_path]
+            exec_subproc(cmd, 1)
+    else:
+        for i in range(max_frame + 1, max_frame + int(fps/10 * 2)):
+            save_path = path + '/img_{0:03d}_{1:05d}.jpg'.format(episode, int(i))
+            cmd = ['cp', image_path, save_path]
+            exec_subproc(cmd, 1)
+
+
+def remove_duplicates(image_path, episode, min_frame, max_frame, type_):
+    path = '/'.join(image_path.split('/')[:-1])
+    if type_ == 0:
+        for i in range(max(0, min_frame - fps * 2), min_frame):
+            save_path = path + '/img_{0:03d}_{1:05d}.jpg'.format(episode, int(i))
+            cmd = ['rm', save_path]
+            exec_subproc(cmd, 1)
+    else:
+        for i in range(max_frame + 1, max_frame + int(fps/10 * 2)):
+            save_path = path + '/img_{0:03d}_{1:05d}.jpg'.format(episode, int(i))
+            cmd = ['rm', save_path]
+            exec_subproc(cmd, 1)
+
+
+
 def get_directory(path, list_episodes):
     video_dict = {}
     file_list = sorted(listdir(path + '/af_jpg'))
@@ -34,21 +64,42 @@ def get_directory(path, list_episodes):
         s_f = f.split('_')
         ep_index = int(s_f[1])
         if ep_index in list_episodes and not ep_index in video_dict.keys():
-            print (f)
-            max_ep = ep_index
-            min_ep = ep_index
-            for f_ in list_episodes:
+            #print (f)
+            max_ep = int (s_f[-1].split('.')[0] )
+            min_ep = int (s_f[-1].split('.')[0] )
+
+            for f_ in file_list:
+                sf2 = f_.split('_')[2]
+                sf3 = sf2.split('.')[0]
+                #print(sf3)
+                fr_index = int(sf3)
                 ep_index_ = int(f_.split('_')[1])
+                # print fr_index
+                # print f_
                 if ep_index_ == ep_index:
-                    if ep_index_ >= max_ep:
-                        max_ep = ep_index_
-            max_ep = max_ep - 120
+
+                    if fr_index >= max_ep:
+                        max_ep = fr_index
+                    if fr_index <= min_ep:
+                        min_ep = fr_index
+            # print('min', min_ep)
+            # print('max', max_ep)
             frame_index = s_f[-1].split('.')[0]
             video_dict[ep_index] = frame_index
-            cmd = get_conctac_cmd(path, ep_index, frame_index, 0)
+            duplicated_image_path = path + '/af_jpg/img_{0:03d}_{1:05d}.jpg'.format(ep_index, min_ep)
+            duplicate_images(duplicated_image_path, ep_index, min_ep, max_ep, 0)
+            cmd = get_conctac_cmd(path, ep_index, max(0, min_ep - fps * 2), 0)
+            print (cmd)
             exec_subproc(cmd, 1)
-            cmd = get_conctac_cmd(path, ep_index, frame_index, 1)
+            remove_duplicates(duplicated_image_path, ep_index, min_ep, max_ep, 0)
+
+
+            duplicated_image_path = path + '/af_jpg/img_{0:03d}_{1:05d}.jpg'.format(ep_index, max_ep)
+            duplicate_images(duplicated_image_path, ep_index, min_ep, max_ep, 1)
+            cmd = get_conctac_cmd(path, ep_index, min_ep, 1)
+            print (cmd)
             exec_subproc(cmd, 1)
+            remove_duplicates(duplicated_image_path, ep_index, min_ep, max_ep, 1)
 
 
 # def pre_images_copy(image_name, fps, path):
